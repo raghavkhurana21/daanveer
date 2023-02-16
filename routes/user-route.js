1e18d784368459b3a27396af289bc71afeb6d7dc
raghavkhurana21 <raghavkhurana789@gmail.com>
2023-02-16 19:58:31 +0530

    changes

---
+++ routes/user-route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router  = express.Router();
+
+const user_controller = require('../controller/user-controller');
+
+router.get('/about',user_controller.about);
+
+router.get('/contact',user_controller.contact);
+
+router.get('/event',user_controller.event);
+router.get('/contact-us',user_controller.contactUs);
+router.get('/volunteer',user_controller.volunteer);
+
+module.exports = router;
+
+
+
